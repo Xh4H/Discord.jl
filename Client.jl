f51c437c362fd3cb7bac6335f59c58127b03c6ec
Juan <priciel@protonmail.com>
2018-09-28 15:54:30 +0200

    Added Emitter

--- Client.jl
+++ Client.jl
@@ -1,18 +1,34 @@
 module Client
 
     include("./WebSocket/Manager.jl")
+    include("./Utils/Emitter.jl")
+
+    evs = Events()
+
     import .WSManager
 
-    struct Self
+    mutable struct Self
         token::String
     end
 
     function init(token::String)
-         structure = Self(token)
-         connect(structure)
+        structure = Self(token)
+        connect(structure)
     end
 
-    function connect(client::Self)
-        WSManager.start(client)
+    function connect(client1::Self)
+        WSManager.start(client1, Client)
+    end
+
+    function on(name::String, fn::Function)
+        on(evs, name, fn)
+    end
+
+    function send(name)
+        emit(evs, name)
+    end
+
+    function send(name, args...)
+        emit(evs, name, args...)
     end
 end
