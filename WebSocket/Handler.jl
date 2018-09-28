4e813682618a1600caed5d5b9ad843ea2b90104e
TheOnlyArtz <gamesil456@gmail.com>
2018-09-28 15:59:26 +0300

    Fix to the Logger library and the Handler.

--- WebSocket/Handler.jl
+++ WebSocket/Handler.jl
@@ -14,16 +14,10 @@ module WSHandler
         eventName = lowercase(eventName) # We treat event names in lowercase
 
         if eventName == "ready"
-            println("TEST")
-            clientName = "$(content["d"]["username"])#$(content["d"]["discriminator"])" #Miss Julia#9120
-            WSLogger.log("Client is ready $clientName", "Log")
+            username = content["user"]["username"]
+            WSLogger.log("Client is ready as: $username", "Log")
         else
-            log("Unhandled event $eventName:", "Warning")
-        # if eventName == "ready"
-        #     clientDiscordName = "$(content["d"]["username"])#$(content["d"]["discriminator"])" #Miss Julia#9120
-        #     log("Logged in successfully as $clientDiscordName.", "Log")
-        # else
-        #     log("Received unhandled event: $eventName", "Log")
+            WSLogger.log("Unhandled event $eventName:", "Warning")
         end
     end
 
