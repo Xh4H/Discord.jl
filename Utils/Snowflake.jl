af989ad0d4afefdc1c0969bae92ad382fbf8c95e
TheOnlyArtz <gamesil456@gmail.com>
2018-09-29 22:50:12 +0300

    Added a snowflake utility to get unix

---
+++ Utils/Snowflake.jl
@@ -0,0 +1,10 @@
+module Snowflake
+    DISCORD_EPOCH = 1420070400000
+    function getUnix(snowflake::String)
+        snowflake = parse(Int64, snowflake)
+        timestamp = Integer(snowflake >> 22) + DISCORD_EPOCH
+        timestampString = String(timestamp)
+
+        return SubString(timestampString, 1, length(timestampString) - 3))
+    end
+end
