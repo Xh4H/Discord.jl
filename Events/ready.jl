6c07ae1298fc5f44ffc8eb6e5e589649f84f392a
TheOnlyArtz <gamesil456@gmail.com>
2018-09-29 17:42:17 +0300

    Set client.user in ReadyEvent

--- Events/ready.jl
+++ Events/ready.jl
@@ -1,11 +1,10 @@
 module ReadyEvent
 
     include("../Structs/User.jl")
-    function executeEvent(client, content, mainClient)
+    function executeEvent(mainClient, content)
         userData = content["user"] # For some reason the first index is 1 and not 0
         a = User.construct(userData)
-        println(a)
-        println("I'm ready in ReadyEvent")
+        mainClient.setUser(a)
         mainClient.send("READY")
     end
 
