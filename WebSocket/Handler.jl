5edb516c643246fd31452e6dbc1dee12a0850adf
TheOnlyArtz <gamesil456@gmail.com>
2018-09-29 13:53:08 +0300

    Added an event handler (events across separate files)

--- WebSocket/Handler.jl
+++ WebSocket/Handler.jl
@@ -1,44 +1,25 @@
 module WSHandler
     include("Logger.jl")
-    include("../Structs/User.jl")
+    include("../Events/EventExporter.jl")
 
     using Base
     using Dates
     import .WSLogger
-    import .User
     import JSON
 
 
-    function handleEvent(data, mainClient) # content is the key "d"
-        eventName = data["t"]
+    function handleEvent(data, client, mainClient) # content is the key "d"
+        eventName = lowercase(data["t"])
         content = data["d"]
-        #println(content)
-
-        if eventName == "READY"
-            @async begin
-                try
-                    user = content["user"]
-                    username = user["username"]
-                    id = user["id"]
-                    discriminator = user["discriminator"]
-                    avatar = user["avatar"]
-                    bot = user["bot"]
-                    userObject = User.Self(username, id, discriminator, avatar, bot)
-
-                    println("The client's username is $(userObject.username)")
-                catch err
-                    println(err)
-                end
+        accessExporter = uppercasefirst("$(eventName)Event") |> Symbol
+        @async begin
+            try
+                a = getfield(EventExporter, accessExporter)
+                a.executeEvent(client, content)
+            catch err
+                println(err)
+                println("I'm not being able handle $eventName yet")
             end
-
-            WSLogger.log("Client is ready as: $username", "Log")
-            mainClient.send(eventName)
-        elseif eventName == "GUILD_CREATE"
-            mainClient.send(eventName, content)
-        elseif eventName == "MESSAGE_CREATE"
-            mainClient.send(eventName, content)
-        else
-            WSLogger.log("Unhandled event $eventName:", "Warning")
         end
     end
 end
