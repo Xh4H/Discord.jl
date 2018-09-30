91c15ffd6d9d1881c4b83c68a93117cf34971f30
TheOnlyArtz <gamesil456@gmail.com>
2018-09-30 15:45:47 +0300

    Exporting the files

--- src/Julicord.jl
+++ src/Julicord.jl
@@ -1,7 +1,24 @@
 module Julicord
-    import JSON
-    import HTTP
-    import WebSockets
-    import Dates
     include("./Client.jl")
+    include("./API/Request.jl")
+    # Events
+    include("./Events/ready.jl")
+    include("./Events/EventExporter.jl")
+    # Structs
+    include("./Structs/Emoji.jl")
+    include("./Structs/User.jl.jl")
+    include("./Structs/Webhook.jl")
+    # Utils
+    include("./Utils/Snowflake.jl")
+    include("./Utils/Constants.jl")
+
+    export Client,
+    Request,
+    # Events
+    EventExporter, ReadyEvent,
+    # Structs
+    Emoji, User, Webhook,
+    # Utils
+    Snowflake, Constants
+
 end # module
