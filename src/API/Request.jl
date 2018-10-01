27db2f383ae939d05650828f1095eba5745259a6
Juan <priciel@protonmail.com>
2018-10-01 12:13:21 +0200

    Fixed possible 400 errors

--- src/API/Request.jl
+++ src/API/Request.jl
@@ -16,9 +16,17 @@ module Request
         "Content-Type" => "application/json"
     )
 
+    shouldSendPayload = Dict(
+        "PATCH" => true,
+        "POST" => true,
+        "PUT" => true
+    )
+
     function createRequest(method::String, endpoint::String, payload = "", query = "", files = "")
         url = BASE_URL * endpoint
-        payload = payload |> JSON.json
+        if haskey(shouldSendPayload, method) && shouldSendPayload[method]
+            payload = payload |> JSON.json
+        end
 
         # Treat query if exist (add query to url (url?bla=ble&..))
 
@@ -30,23 +38,16 @@ module Request
             response = HTTP.request(method, url, headers, payload)
             return String(response.body) |> JSON.parse
         catch err
-
             if err.response.status == 400
                 return Dict("message" => "400: Bad Request")
+            else
+                return err.response.body |> String |> JSON.parse
             end
-
-            body = err.response.body
-
-            return String(body) |> JSON.parse
         end
     end
 
     function sendMessage(channelID, content)
         payload = Dict("content" => content)
         return createRequest("POST", "/channels/$channelID/messages", payload)
-    end
-
-    function getToken()
-        return token
     end
 end
