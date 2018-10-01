7c49517112b82d36c6482436b41e23f6311b7e73
Juan <priciel@protonmail.com>
2018-10-01 10:24:36 +0200

    Added stuff

--- src/Events/message_create.jl
+++ src/Events/message_create.jl
@@ -1,5 +1,8 @@
 module Message_createEvent
+    import ...Message
+
     function executeEvent(mainClient, content)
+        Message.pretty(content)
         mainClient.send("MESSAGE_CREATE", content)
     end
 end
--- src/Structs/User.jl
+++ src/Structs/User.jl
@@ -1,4 +1,8 @@
 module User
     import ..Request
-    
+
+
+    function get(id)
+        return Request.createRequest("GET", "/users/$id")
+    end
 end
