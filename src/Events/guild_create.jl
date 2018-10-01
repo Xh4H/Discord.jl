ad6b79c63a045022b6ccf4a4c14345b81ede4eec
TheOnlyArtz <gamesil456@gmail.com>
2018-10-01 16:14:27 +0300

    Adeed comment and moved stuff

--- src/Events/guild_create.jl
+++ src/Events/guild_create.jl
@@ -1,11 +1,13 @@
 module Guild_createEvent
     function executeEvent(mainClient, content)
         guildID = content["id"]
-        mainClient.guilds[guildID] = content
 
         members = content["members"]
         channels = content["channels"]
         emojis = content["emojis"]
+
+        # Cache guilds to the client
+        mainClient.guilds[guildID] = content
 
         # Cache users to client
         for member in members
@@ -20,9 +22,12 @@ module Guild_createEvent
             mainClient.channels[channelId] = channel
         end
 
+        # Cache emojis to the client
         for emoji in emojis
             emojiId = emoji["id"]
             mainClient.emojis[emojiId] = emoji
         end
+
+        mainClient.send("GUILD_CREATE", content)
     end
 end
