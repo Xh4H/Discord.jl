a36d73ed3ba0ddce5f803fc56901a199832c5c8e
Juan <priciel@protonmail.com>
2018-10-01 12:19:28 +0200

    Code clarification

--- src/Events/guild_create.jl
+++ src/Events/guild_create.jl
@@ -2,11 +2,12 @@ module Guild_createEvent
     function executeEvent(mainClient, content)
         guildID = content["id"]
         mainClient.guilds[guildID] = content
-
         members = content["members"]
 
-        for i in members
-            mainClient.users[i["user"]["id"]] = i["user"]
+        for member in members
+            memberId = member["user"]["id"]
+            user = member["user"]
+            mainClient.users[memberId] = user
         end
     end
 end
