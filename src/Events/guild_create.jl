49f34a35a10bf667079336aeac0fe095abf9a8b4
TheOnlyArtz <gamesil456@gmail.com>
2018-10-01 13:14:49 +0300

    Cache users to the client

--- src/Events/guild_create.jl
+++ src/Events/guild_create.jl
@@ -1,6 +1,12 @@
 module Guild_createEvent
     function executeEvent(mainClient, content)
-        # println(content)
-        
+        guildID = content["id"]
+        mainClient.guilds[guildID] = content
+
+        members = content["members"]
+
+        for i in members
+            mainClient.users[i["user"]["id"]] = i["user"]
+        end
     end
 end
