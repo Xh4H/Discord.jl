6ed9c0c6fa18843b48ee4e21aa039a6169001631
TheOnlyArtz <gamesil456@gmail.com>
2018-10-01 15:09:28 +0300

    Added some caching

--- src/Events/guild_create.jl
+++ src/Events/guild_create.jl
@@ -2,12 +2,27 @@ module Guild_createEvent
     function executeEvent(mainClient, content)
         guildID = content["id"]
         mainClient.guilds[guildID] = content
-        members = content["members"]
 
+        members = content["members"]
+        channels = content["channels"]
+        emojis = content["emojis"]
+
+        # Cache users to client
         for member in members
             memberId = member["user"]["id"]
             user = member["user"]
             mainClient.users[memberId] = user
+        end
+
+        # Cache channels to client
+        for channel in channels
+            channelId = channel["id"]
+            mainClient.channels[channelId] = channel
+        end
+
+        for emoji in emojis
+            emojiId = emoji["id"]
+            mainClient.emojis[emojiId] = emoji
         end
     end
 end
