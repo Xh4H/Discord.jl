646acaf1947e1e360a5176b6d4e6fc7dffcbd9f1
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-07 02:49:30 -0500

    Update make.jl for new repo

--- docs/make.jl
+++ docs/make.jl
@@ -6,14 +6,14 @@ makedocs(;
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/Julicord/Julicord/blob/{commit}{path}#L{line}",
+    repo="https://github.com/PurgePJ/Julicord/blob/{commit}{path}#L{line}",
     sitename="Julicord",
     authors="TheOnlyArtz <gamesil456@gmail.com>, PurgePJ <sindur.esl@gmail.com>",
     assets=[],
 )
 
 deploydocs(;
-    repo="github.com/Julicord/Julicord",
+    repo="github.com/PurgePJ/Julicord",
     target="build",
     julia="1.0",
     deps=nothing,
