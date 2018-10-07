23793bf6bad6ecc7105dd805f95e0d3399b437ed
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-07 02:01:04 -0500

    Handle disconnects, resumes, etc.

--- src/client.jl
+++ src/client.jl
@@ -11,6 +11,20 @@ const conn_properties = Dict(
     "\$device" => "Julicord",
 )
 
+const OPCODES = Dict(
+    0 => :DISPATCH,
+    1 => :HEARTBEAT,
+    2 => :IDENTIFY,
+    3 => :STATUS_UPDATE,
+    4 => :VOICE_STATUS_UPDATE,
+    6 => :RESUME,
+    7 => :RECONNECT,
+    8 => :REQUEST_GUILD_MEMBERS,
+    9 => :INVALID_SESSION,
+    10 => :HELLO,
+    11 => :HEARTBEAT_ACK,
+)
+
 """
     Client(token::String) -> Client
 
@@ -28,7 +42,7 @@ mutable struct Client
     state::Dict{String, Any}  # TODO: This should be a struct.
     handlers::Dict{Type{<:AbstractEvent}, Vector{Function}}
     hb_chan::Channel
-    el_chan::Channel
+    rl_chan::Channel
     conn::OpenTrick.IOWrapper
 
     function Client(token::String)
@@ -38,6 +52,7 @@ mutable struct Client
             0,
             nothing,
             unix2datetime(0),
+            unix2datetime(0),
             Dict(),
             Dict(),
             Channel(0),
@@ -46,14 +61,13 @@ mutable struct Client
     end
 end
 
-
 """
     open(c::Client)
 
 Log in to the Discord gateway and begin reading events.
 """
-function Base.open(c::Client)
-    isopen(c) && @error "Client is already open"
+function Base.open(c::Client; resume::Bool=false)
+    isopen(c) && error("Client is already open")
 
     # Get the gateway URL and connect to it.
     resp = HTTP.get("$DISCORD_API/gateway")
@@ -62,35 +76,54 @@ function Base.open(c::Client)
     conn = opentrick(WebSockets.open, url)
     c.conn = conn
 
-    # Receive HELLO, get heartbeat interval.
-    data, ok = readjson(conn)
-    ok || error("reading HELLO failed")
+    # Receive HELLO.
+    data, e = readjson(conn)
+    e === nothing || throw(e)
+    op = get(OPCODES, data["op"], data["op"])
+    op === :HELLO || error("expected opcode HELLO, received $op")
     hello(c, data)
 
     # Write the first heartbeat.
-    send_heartbeat(c) || error("writing HEARTBEAT failed")
+    heartbeat(c) || error("writing HEARTBEAT failed")
 
     # Read the heartbeat ack.
-    data, ok = readjson(conn)
-    ok || error("reading HEARTBEAT_ACK failed")
+    data, e = readjson(conn)
+    e === nothing || throw(e)
+    op = get(OPCODES, data["op"], data["op"])
+    op === :HEARTBEAT_ACK || error("expected opcode HEARTBEAT_ACK, received $op")
     heartbeat_ack(c, data)
 
-    # Write the IDENTIFY.
-    writejson(conn, Dict(
-        "op" => 2,
-        "d" => Dict(
-            "token" => c.token,
-            "properties" => conn_properties,
-        ),
-    )) || error("writing IDENTIFY failed")
+    if resume
+        # Write the RESUME.
+        writejson(conn, Dict(
+            "op" => 6,
+            "d" => Dict(
+                "token" => c.token,
+                "session_id" => get(c.state, "session_id", ""),
+                "seq" => c.heartbeat_seq,
+            ),
+        )) || error("writing RESUME failed")
+    else
+        # Write the IDENTIFY.
+        writejson(conn, Dict(
+            "op" => 2,
+            "d" => Dict(
+                "token" => c.token,
+                "properties" => conn_properties,
+            ),
+        )) || error("writing IDENTIFY failed")
 
-    # Read the READY message, and assign initial state.
-    data, ok = readjson(conn)
-    ok || error("reading READY failed")
-    c.state = data["d"]
+        # Read the READY message, and assign initial state.
+        data, e = readjson(conn)
+        e === nothing || throw(e)
+        op = get(OPCODES, data["op"], data["op"])
+        op === :DISPATCH || error("expected opcode DISPATCH, received $op")
+        data["t"] == "READY" || error("expected event type READY, received $(data["t"])")
+        c.state = data["d"]
+    end
 
     c.hb_chan = Channel(ch -> maintain_heartbeat(c, ch))
-    c.el_chan = Channel(ch -> event_loop(c, ch))
+    c.rl_chan = Channel(ch -> read_loop(c, ch))
 
     return nothing
 end
@@ -100,7 +133,7 @@ Base.isopen(c::Client) = isdefined(c, :conn) && isopen(c.conn)
 function Base.close(c::Client)
     isdefined(c, :conn) || return
     close(c.hb_chan)
-    close(c.el_chan)
+    close(c.rl_chan)
     close(c.conn)
 end
 
@@ -141,42 +174,31 @@ Removes all handlers for the given event type.
 """
 clear_handlers!(c::Client, event::Type{<:AbstractEvent}) = delete!(c.handlers, event)
 
-# Heartbeat maintenance.
-
-function send_heartbeat(c::Client)
-    ok = writejson(c.conn, Dict("op" => 1, "d" => c.heartbeat_seq))
-    if ok
-        c.last_heartbeat = now()
-    end
-    return ok
-end
+# Client maintenance.
 
 function maintain_heartbeat(c::Client, ch::Channel)
-    while isopen(c.conn)
-        if !send_heartbeat(c)
-            isopen(ch) || return
+    while isopen(ch) && isopen(c.conn)
+        if !heartbeat(c) && isopen(ch)
             @error "writing HEARTBEAT failed"
-        else
+        elseif isopen(ch)
             sleep(c.heartbeat_interval / 1000)
         end
     end
 end
 
-# Event loop.
-
-function event_loop(c::Client, ch::Channel)
-    while isopen(c.conn)
-        data, ok = readjson(c.conn)
-        if !ok
-            isopen(ch) || return
-            @error "read from websocket failed"
+function read_loop(c::Client, ch::Channel)
+    while isopen(ch) && isopen(c.conn)
+        data, e = readjson(c.conn)
+        if e !== nothing
+            isopen(ch) || break
+            handle_error(c, e)
         else
             haskey(HANDLERS, data["op"]) && HANDLERS[data["op"]](c, data)
         end
     end
 end
 
-# Event handlers (TODO)
+# Event handlers
 
 function dispatch(c::Client, data::Dict)
     c.heartbeat_seq = data["s"]
@@ -190,10 +212,24 @@ function dispatch(c::Client, data::Dict)
     end
 end
 
-heartbeat(c::Client, ::Dict) = send_heartbeat(c)
+function heartbeat(c::Client, ::Dict=Dict())
+    ok = writejson(c.conn, Dict("op" => 1, "d" => c.heartbeat_seq))
+    if ok
+        c.last_heartbeat = now()
+    end
+    return ok
+end
 
-reconnect(::Client, ::Dict) = nothing
-invalid_session(::Client, ::Dict) = nothing
+function reconnect(c::Client, ::Dict=Dict(); resume::Bool=true)
+    close(c)
+    open(c; resume=resume)
+end
+
+function invalid_session(c::Client, data::Dict)
+    sleep(rand(1:5))
+    reconnect(c; resume=data["d"])
+end
+
 hello(c::Client, data::Dict) = c.heartbeat_interval = data["d"]["heartbeat_interval"]
 heartbeat_ack(c::Client, ::Dict) = c.last_ack = now()
 
@@ -207,11 +243,75 @@ const HANDLERS = Dict(
     11 => heartbeat_ack,
 )
 
+# Error handling
+
+const CLOSE_CODES = Dict(
+    4000 => :UNKNOWN_ERROR,
+    4001 => :UNKNOWN_OPCODE,
+    4002 => :DECODE_ERROR,
+    4003 => :NOT_AUTHENTICATED,
+    4004 => :AUTHENTICATION_FAILED,
+    4005 => :ALREADY_AUTHENTICATED,
+    4007 => :INVALID_SEQ,
+    4008 => :RATE_LIMITED,
+    4009 => :SESSION_TIMEOUT,
+    4010 => :INVALID_SHARD,
+    4011 => :SHARDING_REQUIRED,
+)
+
+function handle_error(c::Client, e::Exception)
+    if isa(e, WebSocketClosedError)
+        handle_close(c, e)
+    else
+        @error sprint(showerror, e)
+    end
+end
+
+function handle_close(c::Client, e::Exception)
+    code = closecode(e)
+    code === nothing && return nothing
+    err = get(CLOSE_CODES, code, :UNKNOWN_ERROR)
+
+    if err === :UNKNOWN_ERROR
+        reconnect(c)
+    elseif err === :UNKNOWN_OPCODE
+        reconnect(c)
+    elseif err === :DECODE_ERROR  # Probably a library bug.
+        reconnect(c)
+    elseif err === :NOT_AUTHENTICATED  # Probably a library bug.
+        reconnect(c)
+    elseif err === :AUTHENTICATION_FAILED
+        error("WebSocket connection was closed: $code $err")
+    elseif err === :ALREADY_AUTHENTICATED  # Probably a library bug.
+        reconnect(c)
+    elseif err === :INVALID_SEQ  # Probably a library bug.
+        reconnect(c)
+    elseif err === :RATE_LIMITED  # Probably a library bug.
+        @warn "WebSocket connection was closed: $code $err (reconnecting)"
+        reconnect(c)
+    elseif err === :SESSION_TIMEOUT
+        reconnect(c)
+    elseif err === :INVALID_SHARD
+        error("WebSocket connection was closed: $code $err (sharding is not implemented)")
+    elseif err === :SHARDING_REQUIRED
+        error("WebSocket connection was closed: $code $err (sharding is not implemented)")
+    end
+end
+
 # Helpers
 
 function readjson(conn)
-    json, ok = readguarded(conn)
-    ok ? JSON.parse(String(json)) : nothing, ok
+    return try
+        json = read(conn)
+        JSON.parse(String(json)), nothing
+    catch e
+        nothing, e
+    end
 end
 
 writejson(conn, body) = writeguarded(conn, JSON.json(body))
+
+function closecode(e::WebSocketClosedError)
+    m = match(r"OPCODE_CLOSE (\d+)", e.message)
+    return match === nothing ? nothing : parse(Int, String(first(m.captures)))
+end
