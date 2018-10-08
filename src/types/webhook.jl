fd506938c55321afb4bfd484038bd21e1b95b794
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-08 09:27:25 -0500

    Webhook user is nullable, fix doc deployment

--- src/types/webhook.jl
+++ src/types/webhook.jl
@@ -1,12 +1,12 @@
 """
 A Webhook.
-More details [here](https://discordapp.com/developers/docs/resources/webhook#webhook-object).
+More details [here](https://discordapp.com/developers/docs/resources/webhook#webhook-objec)t.
 """
 @from_dict struct Webhook
     id::Union{Snowflake, Missing}
     guild_id::Union{Snowflake, Nothing}
     channel_id::Snowflake
-    user::User
+    user::Union{User, Nothing}
     name::String
     avatar::String
     token::String
