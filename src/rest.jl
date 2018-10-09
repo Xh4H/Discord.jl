a7a7ae2fa5bf32781bb7e6a0d04cab5d36085896
Juan <priciel@protonmail.com>
2018-10-09 21:00:35 +0200

    Update rest.jl

--- src/rest.jl
+++ src/rest.jl
@@ -1,4 +1,5 @@
 include(joinpath("API", "request.jl"))
 # Rest
 include(joinpath("API/rest", "message.jl"))
-include(joinpath("API/rest", "invite.jl"))
+# Not finished yet
+# include(joinpath("API/rest", "invite.jl"))
