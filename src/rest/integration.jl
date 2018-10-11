ad0735227ea91d8d73e61460639f57f41a3637b1
Juan - PurgePJ <priciel@protonmail.com>
2018-10-12 00:11:20 +0200

    Added type permutation for Integrations

--- src/rest/integration.jl
+++ src/rest/integration.jl
@@ -4,8 +4,11 @@ export modify_integration,
 
 """
     modify_integration(c::Client, integration::Integer, guild::Integer, params...) -> Response{Integration}
+    modify_integration(c::Client, integration::Integration, guild::Guild, params...) -> Response{Integration}
+    modify_integration(c::Client, integration::Integration, guild::Integer, params...) -> Response{Integration}
+    modify_integration(c::Client, integration::Integer, guild::Guild, params...) -> Response{Integration}
 
-Modify an [`Integration`](@ref) in the given guild with the given parameters.
+Modify an [`Integration`](@ref) in the given guild.
 
 # Keywords
 - `expire_behavior::Integer`: The behavior when an integration subscription lapses.
@@ -16,8 +19,15 @@ function modify_integration(c::Client, integration::Integer, guild::Integer, par
     return Response{Integration}(c, :PATCH, "/guilds/$guild/integrations/$integration"; body=params)
 end
 
+modify_integration(c::Client, integration::Integration, guild::Guild, params...) = modify_integration(c, integration.id, guild.id, params)
+modify_integration(c::Client, integration::Integration, guild::Integer, params...) = modify_integration(c, integration.id, guild, params)
+modify_integration(c::Client, integration::Integer, guild::Guild, params...) = modify_integration(c, integration, guild.id, params)
+
 """
     sync_integration(c::Client, integration::Integer, guild::Integer) -> Response{Nothing}
+    sync_integration(c::Client, integration::Integration, guild::Guild) -> Response{Nothing}
+    sync_integration(c::Client, integration::Integration, guild::Integer) -> Response{Nothing}
+    sync_integration(c::Client, integration::Integer, guild::Guild) -> Response{Nothing}
 
 Sync an [`Integration`](@ref) in the given guild.
 """
@@ -25,11 +35,22 @@ function sync_integration(c::Client, integration::Integer, guild::Integer)
     return Response{Nothing}(c, :POST, "/guilds/$guild/integrations/$integration/sync")
 end
 
+sync_integration(c::Client, integration::Integration, guild::Guild) = sync_integration(c, integration.id, guild.id)
+sync_integration(c::Client, integration::Integration, guild::Integer) = sync_integration(c, integration.id, guild)
+sync_integration(c::Client, integration::Integer, guild::Guild) = sync_integration(c, integration, guild.id)
+
 """
     delete_integration(c::Client, integration::Integer, guild::Integer) -> Response{Nothing}
+    delete_integration(c::Client, integration::Integration, guild::Guild) -> Response{Nothing}
+    delete_integration(c::Client, integration::Integration, guild::Integer) -> Response{Nothing}
+    delete_integration(c::Client, integration::Integer, guild::Guild) -> Response{Nothing}
 
 Delete an [`Integration`](@ref) in the given guild.
 """
 function delete_integration(c::Client, integration::Integer, guild::Integer)
     return Response{Nothing}(c, :DELETE, "/guilds/$guild/integrations/$integration")
 end
+
+delete_integration(c::Client, integration::Integration, guild::Guild) = delete_integration(c, integration.id, guild.id)
+delete_integration(c::Client, integration::Integration, guild::Integer) = delete_integration(c, integration.id, guild)
+delete_integration(c::Client, integration::Integer, guild::Guild) = delete_integration(c, integration, guild.id)
