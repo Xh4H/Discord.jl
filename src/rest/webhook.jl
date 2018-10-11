75f175b7a33631fa84dcc69340777513030d383a
Juan - PurgePJ <priciel@protonmail.com>
2018-10-12 00:28:21 +0200

    Webhook type permutations

--- src/rest/webhook.jl
+++ src/rest/webhook.jl
@@ -6,8 +6,9 @@ export get_webhook,
     execute_github
 
 """
-    get_webhook(c::Client, webhook::Integer) -> Response{Webhook}
-    get_webhook(c::Client, webhook::Integer, token::AbstractString) -> Response{Webhook}
+    get_webhook(c::Client, webhook::Union{Webhook, Integer}) -> Response{Webhook}
+
+    get_webhook(c::Client, webhook::Union{Webhook, Integer}, token::AbstractString) -> Response{Webhook}
 
 Get a [`Webhook`](@ref).
 """
@@ -19,11 +20,15 @@ function get_webhook(c::Client, webhook::Integer, token::AbstractString)
     return Response{Webhook}(c, :GET, "/webhooks/$webhook/$token")
 end
 
+get_webhook(c::Client, webhook::Webhook) = get_webhook(c, webhook.id)
+get_webhook(c::Client, webhook::Webhook, token::AbstractString) = get_webhook(c, webhook.id, token)
+
 """
-    modify_webhook(c::Client, webhook::Integer; params...) -> Response{Webhook}
+    modify_webhook(c::Client, webhook::Union{Webhook, Integer}; params...) -> Response{Webhook}
+
     modify_webhook(
         c::Client,
-        webhook::Integer,
+        webhook::Union{Webhook, Integer},
         token::AbstractString;
         params...
     ) -> Response{Webhook}
@@ -51,9 +56,13 @@ function modify_webhook(c::Client, webhook::Integer, token::AbstractString; para
     return Response{Webhook}(c, :PATCH, "/webhooks/$webhook/$token"; body=params)
 end
 
+modify_webhook(c::Client, webhook::Webhook; params...) = modify_webhook(c, webhook.id, params)
+modify_webhook(c::Client, webhook::Webhook, token::AbstractString; params...) = modify_webhook(c, webhook.id, token, params)
+
 """
-    delete_webhook(c::Client, webhook::Integer) -> Response{Nothing}
-    delete_webhook(c::Client, webhook::Integer, token::AbstractString) -> Response{Nothing}
+    delete_webhook(c::Client, webhook::Union{Webhook, Integer}) -> Response{Nothing}
+
+    delete_webhook(c::Client, webhook::Union{Webhook, Integer}, token::AbstractString) -> Response{Nothing}
 
 Delete the given [`Webhook`](@ref).
 """
@@ -65,10 +74,13 @@ function delete_webhook(c::Client, webhook::Integer, token::AbstractString)
     return Response{Nothing}(c, :DELETE, "/webhooks/$webhook/$token")
 end
 
+delete_webhook(c::Client, webhook::Webhook) = delete_webhook(c, webhook.id)
+delete_webhook(c::Client, webhook::Webhook, token::AbstractString) = delete_webhook(c, webhook.id, token)
+
 """
     execute_webhook(
         c::Client,
-        webhook::Integer,
+        webhook::Union{Webhook, Integer},
         token::AbstractString;
         wait::Bool=false,
         params...,
@@ -91,7 +103,7 @@ function execute_webhook(
     webhook::Integer,
     token::AbstractString;
     wait::Bool=false,
-    params...,
+    params...
 )
     return if wait
         Response{Message}(c, :POST, "/webhooks/$webhook/$token"; body=params, wait=wait)
@@ -100,10 +112,39 @@ function execute_webhook(
     end
 end
 
+execute_webhook(c::Client, webhook::Webhook, token::AbstractString; wait::Bool=false, params...) = execute_webhook(c, webhook.id, token; wait=wait, params)
+
+"""
+    execute_github(
+        c::Client,
+        webhook::Union{Webhook, Integer},
+        token::AbstractString;
+        wait::Bool=true,
+        params...,
+    ) -> Response{Union{Message, Nothing}}
+
+Execute the given *Github* [`Webhook`](@ref).
+"""
+function execute_github(
+    c::Client,
+    webhook::Integer,
+    token::AbstractString;
+    wait::Bool=true,
+    params...,
+)
+    return if wait
+        Response{Message}(c, :POST, "/webhooks/$webhook/$token/github"; body=params, wait=wait)
+    else
+        Response{Nothing}(c, :POST, "/webhooks/$webhook/$token/github"; body=params)
+    end
+end
+
+execute_github(c::Client, webhook::Webhook, token::AbstractString; wait::Bool=false, params...) = execute_github(c, webhook.id, token; wait=wait, params)
+
 """
     execute_slack(
         c::Client,
-        webhook::Integer,
+        webhook::Union{Webhook, Integer},
         token::AbstractString;
         wait::Bool=true,
         params...,
@@ -125,27 +166,4 @@ function execute_slack(
     end
 end
 
-"""
-    execute_github(
-        c::Client,
-        webhook::Integer,
-        token::AbstractString;
-        wait::Bool=true,
-        params...,
-    ) -> Response{Union{Message, Nothing}}
-
-Execute the given *Github* [`Webhook`](@ref).
-"""
-function execute_github(
-    c::Client,
-    webhook::Integer,
-    token::AbstractString;
-    wait::Bool=true,
-    params...,
-)
-    return if wait
-        Response{Message}(c, :POST, "/webhooks/$webhook/$token/github"; body=params, wait=wait)
-    else
-        Response{Nothing}(c, :POST, "/webhooks/$webhook/$token/github"; body=params)
-    end
-end
+execute_slack(c::Client, webhook::Webhook, token::AbstractString; wait::Bool=false, params...) = execute_slack(c, webhook.id, token; wait=wait, params)
