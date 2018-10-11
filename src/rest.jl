f18ded285d48daf119b0c075a49c530aab6da66c
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-11 00:10:37 -0500

    Introduce Response type for REST functions

--- src/rest.jl
+++ src/rest.jl
@@ -4,46 +4,69 @@ const request_headers = Dict(
 )
 
 const should_send = Dict(
-    "PATCH" => true,
-    "POST" => true,
-    "PUT" => true,
+    :PATCH => true,
+    :POST => true,
+    :PUT => true,
 )
 
-function request(
+struct Response{T}
+    val::Union{T, Nothing}
+    success::Bool
+    cache_hit::Bool
+    http_response::Union{HTTP.Messages.Response, HTTP.ExceptionRequest.StatusError, Nothing}
+end
+
+function Response{Nothing}(e::HTTP.ExceptionRequest.StatusError)
+    return Response{Nothing}(nothing, false, false, e)
+end
+
+function Response{T}(e::HTTP.ExceptionRequest.StatusError) where T
+    return Response{T}(nothing, false, false, e)
+end
+
+function Response{Nothing}(r::HTTP.Messages.Response)
+    return Response{Nothing}(nothing, true, false, r)
+end
+
+function Response{T}(r::HTTP.Messages.Response) where T
+    body = JSON.parse(String(r.body))
+    val, TT = body isa Vector ? (T.(body), Vector{T}) : (T(body), T)
+    Response{TT}(val, true, false, r)
+end
+
+function Response{T}(val::T) where T
+    return Response{T}(val, true, true, nothing)
+end
+
+function Response(c::Client, method::Symbol, endpoint::AbstractString; body::Dict=Dict(), params...)
+    return Response{Nothing}(c, mthod, endpoint; body=body, params...)
+end
+
+function Response{T}(
     c::Client,
-    method::String,
-    endpoint::String;
-    payload=Dict(),
-    query=Dict(),
-)
+    method::Symbol,
+    endpoint::AbstractString;
+    body::Dict=Dict(),
+    params...,
+) where T
     url = DISCORD_API * endpoint
-
-    if !isempty(query)
-        url *= "?$(HTTP.escapeuri(query))"
+    if !isempty(params)
+        url *= "?" * HTTP.escapeuri(params)
     end
 
     headers = copy(request_headers)
     headers["Authorization"] = c.token
 
+    args = [method, url, headers]
+    get(should_send, method, false) && push!(args, json(body))
     return try
-        resp = if get(should_send, method, false)
-            HTTP.request(method, url, headers, json(payload))
-        else
-            HTTP.request(method, url, headers)
-        end
-        false, JSON.parse(String(resp.body))
+        Response{T}(HTTP.request(args...))
     catch e
-        # TODO: Check the type of the exception properly.
-        if isdefined(e, :response) && e.response != nothing
-            if e.response.status == 400
-                true, Dict("message" => "400: Bad Request")
-            else
-                true, JSON.parse(String(e.response.body))
-            end
-        end
+        e isa HTTP.ExceptionRequest.StatusError || rethrow(e)
+        Response{T}(e)
     end
 end
 
 include(joinpath("rest", "invite.jl"))
-include(joinpath("rest", "channel.jl")) # Must be below invite
+include(joinpath("rest", "channel.jl"))
 include(joinpath("rest", "message.jl"))
