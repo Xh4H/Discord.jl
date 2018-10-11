cf845ff639481541d85d4c9cdc1eaddebc6a44a5
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-11 00:31:53 -0500

    Update message.jl too

--- src/rest/message.jl
+++ src/rest/message.jl
@@ -7,104 +7,107 @@ export reply,
     get_reactions,
     delete_reactions
 
-using HTTP: escapeuri
-
 """
-    reply(c::Client, m::Message, content::Union{String, Dict}) -> Message
+    reply(c::Client, m::Message, content::AbstractString) -> Response{Message}
 
-Reply to the given [`Message`](@ref) with the given content
-upon success or a Dict containing error information.
+Reply to the given [`Message`](@ref) (send a message to the same channel).
 """
-function reply(c::Client, m::Message, content::Union{String, Dict})
-    payload = Dict()
-
-    if isa(content, String)
-        payload = Dict("content" => content)
-    elseif isa(content, Dict)
-        payload = content
-        # TODO Handle file uploading here
-    end
-
-    err, data = request(c, "POST", "/channels/$(m.channel_id)/messages"; payload=payload)
-
-    return if err
-        data
-    else
-        Message(data)
-    end
+function reply(c::Client, m::Message, content::Union{AbstractString, Dict})
+    return send_message(c, m.channel.id, content)
 end
 
 """
-    edit(c::Client, m::Message, content) -> Message
+    edit(c::Client, m::Message, content::AbstractString) -> Response{Message}
+    edit(c::Client, m::Message, content::Dict) -> Response{Message}
 
-Edit the given [`Message`](@ref) with the given content
-upon success or a Dict containing error information.
+Edit the given [`Message`](@ref).
 """
-function edit(c::Client, m::Message, content::Union{String, Dict})
-    payload = Dict()
-
-    if isa(content, String)
-        payload = Dict("content" => content)
-    elseif isa(content, Dict)
-        payload = content
+function edit(c::Client, m::Message, content::AbstractString)
+    body = Dict("content" => content)
+    resp = Response{Message}(c, :PATCH, "/channels/$(m.channel_id)/messages"; body=body)
+    if resp.success
+        c.state.messages[resp.val.id] = resp.val
     end
+    return resp
+end
 
-    err, data = request(c, "PATCH", "/channels/$(m.channel_id)/messages/$(m.id)"; payload=payload)
-
-    return if err
-        data
-    else
-        Message(data)
+function edit(c::Client, m::Message, content::Dict)
+    resp = Response{Message}(c, :PATCH, "/channels/$(m.channel_id)/messages/$(m.id)"; body=content)
+    if resp.success
+        c.state.messages[resp.val.id] = resp.val
     end
+    return resp
 end
 
 """
-    delete(c::Client, m::Message) -> Dict
+    delete(c::Client, m::Message) -> Response
 
-Delete the given [`Message`](@ref)
-upon success or a Dict containing error information.
+Delete the given [`Message`](@ref).
 """
-delete(c::Client, m::Message) = request(c, "DELETE", "/channels/$(m.channel_id)/messages/$(m.id)")
-
-"""
-    pin(c::Client, m::Message) -> Dict
-
-Pin the given [`Message`](@ref)
-upon success or a Dict containing error information.
-"""
-pin(c::Client, m::Message) = request(c, "PUT", "/channels/$(m.channel_id)/pins/$(m.id)")
+function delete(c::Client, m::Message)
+    resp = Response(c, :DELETE, "/channels/$(m.channel_id)/messages/$(m.id)")
+    resp.success && delete!(c.state.channels, m.id)
+    return resp
+end
 
 """
-    unpin(c::Client, m::Message) -> Dict
+    pin(c::Client, m::Message) -> Response{Message}
+
+Pin the given [`Message`](@ref).
+"""
+function pin(c::Client, m::Message)
+    resp = Response{Message}(c, :PUT, "/channels/$(m.channel_id)/pins/$(m.id)")
+    if resp.success
+        c.state.messages[m.id] = m
+    end
+    return resp
+end
+
+"""
+    unpin(c::Client, m::Message) -> Response{Message}
 
 Unpin the given [`Message`](@ref)
 upon success or a Dict containing error information.
 """
-unpin(c::Client, m::Message) = request(c, "DELETE", "/channels/$(m.channel_id)/pins/$(m.id)")
+function unpin(c::Client, m::Message)
+    resp = Response{Message}(c, :DELETE, "/channels/$(m.channel_id)/pins/$(m.id)")
+    if resp.success
+        c.state.messages[m.id] = m
+    end
+    return resp
+end
 
 """
-    react(c::Client, m::Message, emoji::String) -> Dict
+    react(c::Client, m::Message, emoji::AbstractString) -> Response
 
-React to the given [`Message`](@ref) with the given emoji
-upon success or a Dict containing error information.
+React to the given [`Message`](@ref).
 """
-react(c::Client, m::Message, emoji::String) = request(c, "PUT", "/channels/$(m.channel_id)/messages/$(m.id)/reactions/$(escapeuri(emoji))/@me")
+function react(c::Client, m::Message, emoji::AbstractString)
+    return Response(
+        c,
+        :PUT,
+        "/channels/$(m.channel_id)/messages/$(m.id)/reactions/$(HTTP.escapeuri(emoji))/@me",
+    )
+end
 
 """
-    get_reactions(c::Client, m::Message, emoji::String) -> Dict
+    get_reactions(c::Client, m::Message, emoji::AbstractString) -> Response{Vector{User}}
 
-Retrieve the users who reacted to the given [`Message`](@ref) with the given emoji
-upon success or a Dict containing error information.
+Get the users who reacted to the given [`Message`](@ref) with the given emoji.
 """
-function get_reactions(c::Client, m::Message, emoji::String)
-    # it is unfinished, need to loop though users
-    err, data = request(c, "GET", "/channels/$(m.channel_id)/messages/$(m.id)/reactions/$(escapeuri(emoji))")
+function get_reactions(c::Client, m::Message, emoji::AbstractString)
+    return Response{User}(
+        c,
+        :GET,
+        "/channels/$(m.channel_id)/messages/$(m.id)/reactions/$(escapeuri(emoji))",
+    )
 end
 
 """
     delete_reactions(c::Client, m::Message) -> Dict
 
-Delete all the reactions from the given [`Message`](@ref)
-upon success or a Dict containing error information.
+Delete all the reactions from the given [`Message`](@ref).
 """
-delete_reactions(c::Client, m::Message) = request(c, "DELETE", "/channels/$(m.channel_id)/messages/$(m.id)/reactions")
+function delete_reactions(c::Client, m::Message)
+    return Response(c, :DELETE, "/channels/$(m.channel_id)/messages/$(m.id)/reactions")
+end
