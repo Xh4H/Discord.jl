3e1117a36f662634df140309f7c65abad47855fc
Juan - PurgePJ <priciel@protonmail.com>
2018-10-12 11:12:06 +0200

    Invite type permutations

--- src/rest/invite.jl
+++ src/rest/invite.jl
@@ -2,7 +2,7 @@ export get_invite,
     delete_invite
 
 """
-    get_invite(c::Client, code::AbstractString; with_counts::Bool=false) -> Response{Invite}
+    get_invite(c::Client, code::Union{Invite, AbstractString}; with_counts::Bool=false) -> Response{Invite}
 
 Get an [`Invite`](@ref) with the given code. If `with_counts` is set, the [`Invite`](@ref)
 will contain approximate member counts.
@@ -12,11 +12,15 @@ function get_invite(c::Client, code::AbstractString; with_counts::Bool=false)
     # TODO: Same as create_invite.
 end
 
+get_invite(c::Client, inv::Invite; with_counts::Bool=false) = get_invite(c, inv.code; with_counts=with_counts)
+
 """
-    delete_invite(c::Client, code::AbstractString) -> Response{Invite}
+    delete_invite(c::Client, code::Union{Invite, AbstractString}) -> Response{Invite}
 
 Delete an [`Invite`](@ref) with the given code.
 """
 function delete_invite(c::Client, code::AbstractString)
     return Response{Invite}(c, :DELETE, "/invites/$code")
 end
+
+delete_invite(c::Client, inv::Invite) = delete_invite(c, inv.code)
