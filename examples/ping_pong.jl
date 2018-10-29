4260b0e5bc216026122f3d4ee473e43e53fa4c71
PurgePJ <priciel@protonmail.com>
2018-10-29 10:18:56 +0100

    Changed example function name
    MessageCreate will execute "on_message_create" every time a new message comes, we want to make it look like that, and not like it will only work for "pong".

--- examples/ping_pong.jl
+++ examples/ping_pong.jl
@@ -2,13 +2,13 @@ module PingPong
 
 using Discord
 
-function pong(c::Client, e::MessageCreate)
+function on_message_create(c::Client, e::MessageCreate)
     e.message.content == "ping" && reply(c, e.message, "pong")
 end
 
 function main()
     c = Client(ENV["DISCORD_TOKEN"])
-    add_handler!(c, MessageCreate, pong)
+    add_handler!(c, MessageCreate, on_message_create)
     open(c)
     wait(c)
 end
