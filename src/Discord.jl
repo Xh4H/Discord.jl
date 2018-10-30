242ea33fc13ef250745de3e1f19e3ff87bf9534f
Chris de Graaf <chrisadegraaf@gmail.com>
2018-10-29 22:57:26 -0500

    Separate gateway code from Client
    Much easier to split up what needs to be tested now.

--- src/Discord.jl
+++ src/Discord.jl
@@ -11,11 +11,21 @@ using WebSockets
 const API_VERSION = 6
 const DISCORD_API = "https://discordapp.com/api"
 
+# Generic utils.
+
+function locked(f::Function, l::Threads.AbstractLock)
+    lock(l)
+    try f() finally unlock(l) end
+end
+
+insert_or_update(d, k, v) = d[k] = haskey(d, k) ? merge(d[k], v) : v
+
 include("types.jl")
 include("events.jl")
 include("state.jl")
 include("limiter.jl")
 include("client.jl")
+include("gateway.jl")
 include("rest.jl")
 include("commands.jl")
 include("Defaults.jl")
