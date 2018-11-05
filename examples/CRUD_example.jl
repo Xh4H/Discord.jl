5d1e3d40a9267068b4d99c60f61fcccb39432633
Juan <priciel@protonmail.com>
2018-11-05 15:34:38 +0100

    Fixed comments and improved code example

--- examples/CRUD_example.jl
+++ examples/CRUD_example.jl
@@ -25,19 +25,25 @@ function main()
     # · Webhook
 
     # CRUD format:
-    # create(Client, CRUDType, param 1[, param n...])
-    # retrieve(Client, CRUDType, param 1[, param n...])
-    # update(Client, CRUDType, param 1[, param n...])
-    # delete(Client, CRUDType, param 1[, param n...])
+    # create(Client, CRUDType, param 1[, param n...][;kwargs])
+    # retrieve(Client, CRUDType, param 1[, param n...][;kwargs])
+    # update(Client, param 1[, param n...][;kwargs])
+    # delete(Client, param 1[, param n...])
 
     # Retrieving a channel
     test_channel = retrieve(c, DiscordChannel, 508717799405781003) # -> Distributed.Future
     # Returns a Future, to fetch the value:
     channel_val = fetch(test_channel).val # We get the value from the fetched Response
     channel_name = channel_val.name
+    # Or a synonim of the above
+    channel_name = fetchval(test_channel).name
 
     # Deleting a guild
-    delete(c, Guild, 81384788765712384)
+    guild_to_delete = get_guild(c, 81384788765712384)
+    delete(c, fetchval(guild_to_delete))
+    # Or a synonym of the above
+    delete_guild(c, 81384788765712384)
+    
     return c
 end
 
