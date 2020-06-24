a4207e9faa583f711542462b2c96586a8249f24c
Christopher Lozinski <lozinski@PythonLinks.info>
2020-06-24 05:53:16 +0000

    Actually added the file.

---
+++ examples/pingpong.jl
@@ -0,0 +1,25 @@
+module Ping
+
+using Discord
+
+function on_message_create(client::Client, event::MessageCreate)
+    if event.message.content == "ping"
+       reply(client, event.message, "Pong!")    
+    elseif event.message.content == "pong"   	  
+       reply(client, event.message, "Ping!")
+    end
+end
+
+function main()
+    c = Client(ENV["DISCORD_TOKEN"])
+    open(client)
+    add_handler!(client, MessageCreate, on_message_create; tag=:ping)
+    return client
+end
+
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    client = Ping.main()
+    wait(client)
+end
