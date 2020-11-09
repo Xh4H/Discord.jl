63e62c5ccac9bbf9e5a77fd0536bff8c13d47db6
nxtlo <dhmony-99@hotmail.com>
2020-11-09 23:23:11 +0300

    Embed example

---
+++ examples/embed.jl
@@ -0,0 +1,48 @@
+module Embed
+
+using Discord
+
+function embed(c::Client, m::Message)
+
+    time_now = Dates.now(Dates.UTC)
+    local e = Embed(
+        
+        color = 0x36393f,
+        title = "Embed title",
+        description="[Embed description](https://github.com/Xh4H/Discord.jl) $(string(m.author))",
+
+        fields=[EmbedField(
+                    name="Field name 1", value="Field value 1", inline=true),
+                EmbedField(
+                    name="Field name 2", value="Field value 2", inline=false),
+                EmbedField(
+                    name="Field name 3", value="Field value 3"),
+                EmbedField(
+                    name="Field name 4", value="[Field value 4](https://github.com/Xh4H/Discord.jl)")
+                ],
+        
+        author=EmbedAuthor(
+            name="Embed author",
+            icon_url="https://cdn.discordapp.com/embed/avatars/0.png"),
+
+        thumbnail=EmbedThumbnail(
+            url="https://cdn.discordapp.com/embed/avatars/0.png"),
+        
+        image=EmbedImage(
+            url="https://cdn.discordapp.com/embed/avatars/0.png"),
+        
+        footer=EmbedFooter(
+            text="Embed footer",
+            icon_url="https://cdn.discordapp.com/embed/avatars/0.png"
+    ))
+    
+    reply(c, m, e)
+end
+
+
+@command(
+    name=:embed,
+    handler=embed,
+    help="Simple embed",
+);
+end
